8e806e4e499a429df6609c84c8bf3a1ad03d73bb
varunsonawane62-alt <your-github-varunsonawane62@gmail.com>
2026-09-23 00:10:33 +0530

    new file

---
+++ Assignment03.py
@@ -0,0 +1,11 @@
+
+assignments = ["Math Homework", "Physics Lab", "English Essay"]
+
+
+assignments.append("History Presentation")
+
+
+assignments.remove("Physics Lab")
+
+
+print("Updated Assignments:", assignments)
